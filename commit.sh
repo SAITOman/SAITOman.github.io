1e0d3b32c0e4ba69a531c7203b36299f4e3c835c
sAITOman <b1018114@fun.ac.jp>
2018-11-29 05:17:45 +0000

    Updata pages

--- commit.sh
+++ commit.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 git add --all
-if[$1 -eq 0]then
-    git commit -m "Initial commit"
-    echo $1
-elif[$1 -eq 1]then
-    git commit -m "Updata pages"
-    echo $1
-fi
+git commit -m "Updata pages"
 git push -u origin master
 exit 0
