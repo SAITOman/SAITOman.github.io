7fc3d410098fb367993552c8e8972b795e36178f
sAITOman <b1018114@fun.ac.jp>
2018-11-29 05:16:05 +0000

    Initial commit

--- commit.sh
+++ commit.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 git add --all
-if["$1" -eq 0] then
+if[$1 -eq 0]then
     git commit -m "Initial commit"
     echo $1
-elif["$1" -eq 1] then
+elif[$1 -eq 1]then
     git commit -m "Updata pages"
     echo $1
 fi
